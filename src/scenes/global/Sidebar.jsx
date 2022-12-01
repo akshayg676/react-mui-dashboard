00d153be4875c4f38d4e67621436efe1f5c7edf5
AkshayG-accruon <akshay@accruontechnologies.co.in>
2022-12-01 17:01:19 +0530

    update sidebar details

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -90,33 +90,6 @@ const Sidebar = () => {
             )}
           </MenuItem>
 
-          {!isCollapsed && (
-            <Box mb="25px">
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <img
-                  alt="profile-user"
-                  width="100px"
-                  height="100px"
-                  src={userImg}
-                  style={{ cursor: "pointer", borderRadius: "50%" }}
-                />
-              </Box>
-              <Box textAlign="center">
-                <Typography
-                  variant="h2"
-                  color={colors.grey[100]}
-                  fontWeight="bold"
-                  sx={{ m: "10px 0 0 0" }}
-                >
-                  John Smith
-                </Typography>
-                <Typography variant="h5" color={colors.yellowAccent[500]}>
-                  Admin
-                </Typography>
-              </Box>
-            </Box>
-          )}
-
           <Box paddingLeft={isCollapsed && "10%"}>
             <Item
               title="Dashboard"
